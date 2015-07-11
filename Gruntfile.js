e7f170a4bf2eae5fa12fefb12467ac4a23c7f586
Rain Wu <rainlonely@me.com>
2015-07-11 17:19:15 +0800

    add new & edit adviser management page

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,8 @@ module.exports = function (grunt) {
                     ".tmp/styles/rain_form.css": "<%= web.app %>/assets/less/rain_form.less",
                     ".tmp/styles/projectlist.css": "<%= web.app %>/assets/less/projectlist.less",
                     ".tmp/styles/newcrowd.css": "<%= web.app %>/assets/less/newcrowd.less",
-                    ".tmp/styles/report.css": "<%= web.app %>/assets/less/report.less"
+                    ".tmp/styles/report.css": "<%= web.app %>/assets/less/report.less",
+                    ".tmp/styles/adviser_edit.css": "<%= web.app %>/assets/less/adviser_edit.less"
                 }
             }
         },
